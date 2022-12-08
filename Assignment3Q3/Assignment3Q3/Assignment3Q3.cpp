4fbacbcea76d115fddee7766076dd00e89c70c85
MiggleK <98611485+MiggleK@users.noreply.github.com>
2022-12-08 10:16:46 -0600

    Assignment3

--- Assignment3Q3/Assignment3Q3/Assignment3Q3.cpp
+++ Assignment3Q3/Assignment3Q3/Assignment3Q3.cpp
@@ -1,10 +1,56 @@
-// Assignment3Q3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+/*
+* Created by: Miguel R. Kunkle
+* Date: 12/7/2022
+* 
+* Description: A map is created by inserting key value pairs of states and their capitals.
+* There is one function to print the map, and another to ask the user which state they wish to see the capital of.
+*/
 #include <iostream>
+#include <string>
+#include <map>
+
+void printMap(std::map<std::string, std::string> stateDataMap)
+{
+    std::map<std::string, std::string>::iterator it;
+
+    for (it = stateDataMap.begin(); it != stateDataMap.end(); it++)
+    {
+        std::cout << "Key: " << it->first << " Value: " << it->second << std::endl;
+    }
+
+}
+
+std::string printValue(std::map<std::string, std::string> stateDataMap)
+{
+    std::string input;
+
+    std::cout << "Please enter the state you wish to know the capital of: ";
+    std::cin >> input;
+
+    return stateDataMap[input];
+
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //create map
+    std::map<std::string, std::string> stateDataMap;
+
+    //fill map with key-value pairs
+    stateDataMap["Nebraska"] = "Lincoln";
+    stateDataMap["New York"] = "Albany";
+    stateDataMap["Ohio"] = "Columbus";
+    stateDataMap["California"] = "Sacramento";
+    stateDataMap["Masshachusetts"] = "Boston";
+    stateDataMap["Texas"] = "Austin";
+
+    //print information in map
+    printMap(stateDataMap);
+
+    //chaing capital of California to Los Angeles
+    stateDataMap["California"] = "Los Angeles";
+
+    //user gets to ask for a capital
+    std::cout << printValue(stateDataMap) << std::endl;
 }
 
---
+++ Assignment3Q3/Assignment3Q3/CapMap.h
@@ -0,0 +1,13 @@
+#pragma once
+#ifndef CAPMAP_H_
+#define CAPMAP_H_
+
+#include <string>
+#include <map>
+
+void printMap(std::map<std::string, std::string> stateDataMap);
+
+std::string printValue(std::map<std::string, std::string> stateDataMap);
+
+#endif
+
